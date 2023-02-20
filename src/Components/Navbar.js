a349cf40e7f014856fb89647d570a69f6afef2d5
thanura mendis <thanuramendis02@gmail.com>
2023-02-20 12:36:46 +0530

    project setup

---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <h1 className="nav-title">MARKETPLACE</h1>
+        </nav>
+    )
+}
