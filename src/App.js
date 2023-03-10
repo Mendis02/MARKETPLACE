d65c34a0d90796a575cfe8ed5da7650d6f67bcf1
thanura mendis <thanuramendis02@gmail.com>
2023-03-10 18:15:32 +0530

    post.map not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 
-import Navbar from "./Components/Navbar" 
+import Navbar from "./Components/Navbar"
+import PostsMain from "./screens/Posts"
 
-export default function App(){
-    return(
+export default function App() {
+    return (
         <div>
             <Navbar />
             <h1>123</h1>
+            <PostsMain />
         </div>
     )
 }
--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,1 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+const API_ENDPOINT = 'https://dummyjson.com/products'
+
+const PostsMain = () => {
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        getDataFromApi()
+    }, [])
+
+    const getDataFromApi = () => {
+        fetch(API_ENDPOINT)
+            .then((response) => response.json())
+            .then((data) => setPost(data))
+            .catch(err => console.error(err))
+    }
+
+    return (
+        <div>
+            {post?.map(p =>
+                <div key={p.products.id}>
+                    <h1>{p.products.title}</h1>
+                    {/* <img src = {p.thumbnail}/> */}
+                    {/* <h2>{p.price}</h2> */}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default PostsMain;
